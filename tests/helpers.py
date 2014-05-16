ac9fec56739db739a29b904b026dda646888bcc1
Christopher Small <csmall@fhcrc.org>
2014-05-16 13:15:38 -0700

    Huge refactor on test code (yay metaprogramming); will make adding more tests easier

---
+++ tests/helpers.py
@@ -0,0 +1,56 @@
+
+import unittest
+from mutgen import core
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+
+
+def seqrecord(seqstring):
+    "Silly little function for generating a simple seqrecord"
+    sr = SeqRecord('whocares')
+    sr.seq = Seq(seqstring)
+    return sr
+
+
+class TestGenerator(object):
+    """Class for constructing TestCase subclasses with all the logic needed to test impirical frequencies
+    based on a given mutation model."""
+
+    def __init__(self, sample_size, almost_equal_places):
+        """Construct the generator object; sample_size and almost_equal_places can be specified here and used
+        as defaults for all TestCase subclasses generated"""
+
+        self.sample_size = sample_size
+        self.almost_equal_places = almost_equal_places
+
+    def make_test(self, name, testseq, patterns, result_probs, sample_size=None, almost_equal_places=None):
+        """Construct the new class, where `name` is the name of the new class, `testseq` is a sequence which
+        will be tested, `patterns` specifies the mutation patterns (see `core.Mutator.__init__`), and
+        `almost_equal_places` is a dictionary of kmers to expected mutation frequencies."""
+
+        def setUp(test_case):
+            test_case.seqrecords = [seqrecord(testseq) for i in xrange(self.sample_size)]
+            test_case.mutator = core.Mutator(patterns)
+            test_case.sample_size = sample_size or self.sample_size
+            test_case.almost_equal_places = almost_equal_places or self.almost_equal_places
+
+        def test_frequencies(test_case):
+            seqscan = test_case.mutator.seqscan(test_case.seqrecords)
+            results = dict((kmer, 0) for kmer in result_probs)
+            for _, matching_kmers in seqscan:
+                for kmer, mutated in matching_kmers:
+                    if mutated:
+                        try:
+                            results[kmer] += 1
+                        except KeyError:
+                            pass
+            for kmer, count in results.iteritems():
+                freq = float(count) / test_case.sample_size
+                failmsg="Frequency of %s mutation was expected to be %s but was %s (diff: %s)." % (
+                        kmer, freq, result_probs[kmer], abs(freq - result_probs[kmer]))
+                test_case.assertAlmostEqual(freq, result_probs[kmer], places=test_case.almost_equal_places,
+                        msg=failmsg)
+
+        return type(name, (unittest.TestCase,), dict(setUp=setUp, test_frequencies=test_frequencies))
+
+
--- tests/test_validity.py
+++ tests/test_validity.py
@@ -1,41 +1,20 @@
-import unittest
-from mutgen import core
-
-from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
+from helpers import TestGenerator
 
 import random
 random.seed("I can get you a toe dude. With nail polish!")
 
-assert_almost_equal_places = 1
-sample_size = 1000
 
 def mut_pattern(motif, prob):
+    # XXX - need to replace this with something more intelligent
     return [motif, 0, [('A', prob), ('C', 0), ('G', 0), ('T', 0)]]
 
 
-class TestSimpleSinglePattern(unittest.TestCase):
-    def setUp(self):
-        def seqrecord(seqstring):
-            sr = SeqRecord('whocares')
-            sr.seq = Seq(seqstring)
-            return sr
-        self.seqrecords = [seqrecord('CCCGTCCCCGACCCCCGGCCCCC') for i in xrange(sample_size)]
-        self.mutator = core.Mutator(
-                [mut_pattern('GT', 0.2),
-                    mut_pattern('GA', 0.3),
-                    mut_pattern('GG', 0.7)])
+test_generator = TestGenerator(sample_size = 1000, almost_equal_places = 1)
 
-    def test_frequencies(self):
-        seqscan = self.mutator.seqscan(self.seqrecords)
-        results = dict(GT=0, GA=0, GG=0)
-        for _, matching_kmers in seqscan:
-            for kmer, mutated in matching_kmers:
-                if mutated:
-                    results[kmer] += 1
-
-        self.assertAlmostEqual(float(results['GT']) / sample_size, 0.2, places=assert_almost_equal_places)
-        self.assertAlmostEqual(float(results['GA']) / sample_size, 0.3, places=assert_almost_equal_places)
-        self.assertAlmostEqual(float(results['GG']) / sample_size, 0.7, places=assert_almost_equal_places)
-
+TestSimpleSinglePattern = test_generator.make_test("TestSimpleSinglePattern",
+    'CCCGTCCCCGACCCCCGGCCCCC',
+    [mut_pattern('GT', 0.2),
+        mut_pattern('GA', 0.3),
+        mut_pattern('GG', 0.7)],
+    dict(GT=0.2, GA=0.3, GG=0.7))
 
